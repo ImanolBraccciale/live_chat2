dc5084419b7d79fa1b3733f81bc4f266492ae0e7
ImanolBraccciale <imanol11699@outlook.com>
2024-07-06 20:30:56 -0300

    zxcxzc

--- web/src/app/components/auth.tsx
+++ web/src/app/components/auth.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { useState } from 'react';
+import styles from './Login.module.css';
 
 const Login = ({ onLogin }: { onLogin: (username: string) => void }) => {
   const [username, setUsername] = useState('');
@@ -12,15 +13,16 @@ const Login = ({ onLogin }: { onLogin: (username: string) => void }) => {
   };
 
   return (
-    <div >
+    <div className={styles.loginContainer}>
       <h1>Enter your username</h1>
       <input
         type="text"
         value={username}
         onChange={(e) => setUsername(e.target.value)}
         placeholder="Username"
+        className={styles.input}
       />
-      <button onClick={handleLogin} >Login</button>
+      <button onClick={handleLogin} className={styles.button}>Login</button>
     </div>
   );
 };
