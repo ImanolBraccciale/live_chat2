f65ddc69cb2c32fc814660cec3ae8b83940d0d3b
ImanolBraccciale <imanol11699@outlook.com>
2024-07-06 22:07:06 -0300

    asdasd

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://live-chat-1ovi8qqtx-imanols-projects.vercel.app ",
+    origin: "https://live-chat-1ovi8qqtx-imanols-projects.vercel.app",
     credentials: true,
-  });
+  });https://live-chat-1ovi8qqtx-imanols-projects.vercel.app
   await app.listen(4000);
 }
 bootstrap();
