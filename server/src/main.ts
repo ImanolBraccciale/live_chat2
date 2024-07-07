d2cca27b61ae26cceb692397809c530d6665653a
ImanolBraccciale <imanol11699@outlook.com>
2024-07-06 22:19:35 -0300

    asd

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "https://live-chat-1ovi8qqtx-imanols-projects.vercel.app",
+    origin: 'https://live-chat-1ovi8qqtx-imanols-projects.vercel.app',
     credentials: true,
+    methods: ['GET', 'POST'], // Add the appropriate HTTP methods used by your client
+    allowedHeaders: ['Authorization', 'Content-Type'], // Add headers used by your client
   });
+
   await app.listen(4000);
 }
 bootstrap();
