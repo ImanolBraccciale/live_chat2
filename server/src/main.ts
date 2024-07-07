61a0295c266cf7bab24dcb336e33a7c73bd05a5e
ImanolBraccciale <imanol11699@outlook.com>
2024-07-07 01:24:58 -0300

    asdasdasdasd

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,13 +3,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true,
-    credentials: true,
-    methods: ['GET', 'POST'], // Add the appropriate HTTP methods used by your client
-    allowedHeaders: ['Authorization', 'Content-Type'], // Add headers used by your client
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
   });
-
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   await app.listen(4000);
 }
 bootstrap();
